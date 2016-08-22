45a4d0fec3ed78e38096193c0a5b4c18897acd7d
benkeser <benkeser@berkeley.edu>
2016-08-22 12:35:13 -0700

    fixed print method

--- R/r2weight-print-method.R
+++ R/r2weight-print-method.R
@@ -1,3 +1,6 @@
+#' @method print r2weight    
+#' @export 
+
 print.r2weight <- function(x,...){
     cat("CV-R2 (95% CI) = ", round(x[[1]]$cv.wR2,3)," (", 
         round(x[[1]]$cv.wR2.ci[1],3)," , ",
