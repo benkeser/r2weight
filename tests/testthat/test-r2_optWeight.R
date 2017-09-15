85ec4c1d3791a1239f733654231356a5291e4d30
David Benkeser <benkeser@berkeley.edu>
2017-09-15 09:26:54 -0400

    move sim to sandbox. add r2_diff test

--- tests/testthat/test-r2_optWeight.R
+++ tests/testthat/test-r2_optWeight.R
@@ -26,12 +26,22 @@ test_that("optWeight works as expected.", {
 
 	# call optWeight using simple Super Learner library
 	out1 <- optWeight(Y = Y, X = X, SL.library = c("SL.mean","SL.glm"))
+	out2 <- optWeight(Y = Y, X = X[,-3], SL.library = c("SL.mean","SL.glm"))
 
 	# cross-validated R-squared
 	# set verbose = TRUE to see a progress bar
 	r2.out1 <- r2_optWeight(out1, Y = Y, X = X, evalV = 2, verbose = TRUE)
+	r2.out2 <- r2_optWeight(out1, Y = Y, X = X[,-3], evalV = 2, verbose = TRUE)
 
 	expect_true(!is.na(r2.out1$r2))
 	expect_true(all(!is.na(r2.out1$r2.ci)))
 	expect_true(!is.na(r2.out1$r2.pval))
+
+	# check r2_diff
+	d1 <- r2_diff(out1, out2)
+	expect_true(all(!is.na(unlist(d1[1:3]))))
+	d2 <- r2_diff(r2.out1, r2.out2)
+	expect_true(d2$diff[2] < d2$diff[3])
+	expect_true(all(!is.na(d2$diff)))
+
 })
